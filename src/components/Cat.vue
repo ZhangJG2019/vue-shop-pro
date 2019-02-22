e2d00cd18f3002b039b4fbb530f243f0dd555e99
zhangjuguang <1161401821@qq.com>
2019-02-22 19:34:45 +0800

    权限管理开发完毕

---
+++ src/components/Cat.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 开始-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/welcome' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ path: '/rights' }">商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 面包屑导航区域 结束-->
+    <!-- card卡片区域 开始-->
+    <el-card class="box-card">
+      <el-button type="primary">添加分类</el-button>
+      <!-- table-tree 展示区 开始-->
+	  <zk-table :data="catInfos" :columns="catInfosColumns"></zk-table>
+      <!-- table-tree 展示区 结束-->
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+</style>
